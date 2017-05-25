e020d2adcc3739457e15f61cc4c292ea9c2dc2ce
Johannes Castner <jac2130@gmail.com>
2017-05-25 19:29:56 +0000

    started to split up tables into non-changing and changing variables

--- data_sqlite.py
+++ data_sqlite.py
@@ -41,7 +41,7 @@ for evnt in events:
     category     = str("https://www.predictit.org/Market/" + raw_event["Category"])
     category_name= str(raw_event["Category"])
 
-    values = str(tuple((name,ID_str, ticker, short_name, time_stamp, status, image, category_name, category)))
+    values = str(tuple((name, event_str, ID_str, ticker, short_name, time_stamp, status, image, category_name, category)))
     #print(values)
     cursor.execute("INSERT INTO questions VALUES" + str(values))
 
@@ -69,9 +69,15 @@ for evnt in events:
         best_sell_no = handle_missing(cont["BestSellNoCost"])
         last_close_p = handle_missing(cont["LastClosePrice"])
 
-        values = str(tuple((name,ID, ID_str, ticker, short_name, long_name, date_end, status, image, last_trade_p, best_buy_yes, best_buy_no, best_sell_yes, best_sell_no, last_close_p)))
+        values = str(tuple((name,contract, ID, ID_str, ticker, short_name, long_name,image)))
         #print(values)
         cursor.execute("INSERT INTO contracts VALUES" + str(values))
+        #create time stamp
+        time_stamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        
+        values = str(tuple((ID, time_stamp, date_end, status, last_trade_p, best_buy_yes, best_buy_no, best_sell_yes, best_sell_no, last_close_p)))
+        
+        cursor.execute("INSERT INTO contract_vars VALUES" + str(values))
         conn.commit()
 
 conn.close()
--- intitialize_sqlite_tables.py
+++ intitialize_sqlite_tables.py
@@ -5,9 +5,12 @@ cursor = conn.cursor()
 # Create tables (only need to do this once)
 
 cursor.execute('''CREATE TABLE questions
-             (name text, ID text, ticker text, short_name text, time_stamp text, status text, image_url text, category text, category_url text)''')
+             (name text, IRL text, ID text, ticker text, short_name text, time_stamp text, status text, image_url text, category text, category_url text)''')
 
 cursor.execute('''CREATE TABLE contracts
-             (name text, ID text, question text, ticker text, short_name text, long_name text, date_end text, status text, image_url text, last_trade_price real, best_buy_yes real, best_buy_no real, best_sell_yes real, best_sell_no real, last_close_price real)''')
+             (name text, IRL text, ID text, question text, ticker text, short_name text, long_name text, image_url text)'''
+
+cursor.execute('''CREATE TABLE contract_vars
+               (ID text, time_stamp text, date_end text, status text, last_trade_price real, best_buy_yes real, best_buy_no real, best_sell_yes real, best_sell_no real, last_close_price real)''')
 
 conn.close()
